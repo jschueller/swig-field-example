6862c9d797df5bb948576894869b3611b234847f
Julien Schueller <schueller@phimeca.com>
2018-07-03 13:15:44 +0200

    Rename OpenTURNSFieldFunction

--- test-fieldfunction.py
+++ test-fieldfunction.py
@@ -1,13 +1,12 @@
 import openturns as ot
 
-class OpenTURNSFieldFunction(object):
+class OpenTURNSPythonFieldFunction(object):
     def __init__(self):
-        #super(OpenTURNSFieldFunction).__init__(self)
         self.__mesh = ot.Mesh()
     def getInputMesh(self):
         return self.__mesh
 
-obj = OpenTURNSFieldFunction()
+obj = OpenTURNSPythonFieldFunction()
 
 f = ot.FieldFunction(obj)
 print('OK')
